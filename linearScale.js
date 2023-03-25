39259ecec791ff51ef8948ebb7e57023c35b5140
Ran Pol <rnd809@gmail.com>
2023-03-25 17:31:09 -0400

    Create a linear scale.

--- linearScale.js
+++ linearScale.js
@@ -1,9 +1,8 @@
 // Add your code below this line
 
 const scale = d3.scaleLinear()
-const output = scale() // Call scale with an argument here
+const output = scale(50) // Call scale with an argument here
 
 // Add your code above this line
 
 d3.select('body').append('h2').text(output)
- 
