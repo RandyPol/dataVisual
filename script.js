dee92810e5efd0908cfb47b1b41251d32ac350ad
Ran Pol <rnd809@gmail.com>
2023-03-25 12:25:52 -0400

    Create a Bar for Each Data Point on the Set.

--- script.js
+++ script.js
@@ -5,7 +5,14 @@ const h = 100
 
 const svg = d3.select('body').append('svg').attr('width', w).attr('height', h)
 
+/**
+ * Create a Bar for Each Data Point in the Set
+ */
+
 svg
+  .selectAll('rect')
+  .data(dataset)
+  .enter()
   .append('rect')
   .attr('width', 25)
   .attr('height', 100)
