dcf0bf46b6b572890f5993fffc3da12821a71658
Ran Pol <rnd809@gmail.com>
2023-03-26 12:47:25 -0400

    Add handle click event.

--- d3JsonApi/script.js
+++ d3JsonApi/script.js
@@ -1,1 +1,13 @@
-console.log("I'm connected!")
+// document.addEventListener('DOMContentLoaded', function () {
+//   const button = document.querySelector('#getMessage')
+
+//   button.addEventListener('click', () => {
+//     console.log("KLK")
+//   })
+// })
+
+document.addEventListener('DOMContentLoaded', function () {
+  const button = document.getElementById('getMessage')
+
+  button.onclick = function () {}
+})
