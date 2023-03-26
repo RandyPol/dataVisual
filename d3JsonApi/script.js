92f106fd04aa37ecedb767b5b05e633a8dc3383f
Ran Pol <rnd809@gmail.com>
2023-03-26 13:07:02 -0400

    Get json with the Javascript fetch method.

--- d3JsonApi/script.js
+++ d3JsonApi/script.js
@@ -6,16 +6,28 @@
 //   })
 // })
 
+// document.addEventListener('DOMContentLoaded', function () {
+//   const button = document.getElementById('getMessage')
+
+//   button.onclick = function () {
+//     const req = new XMLHttpRequest()
+//     req.open('GET', '/json/cats.json', true)
+//     req.send()
+//     req.onload = function () {
+//       const json = JSON.parse(req.responseText)
+//       document.getElementsByClassName('message')[0].innerHTML = JSON.stringify(json)
+//     }
+//   }
+// })
+
 document.addEventListener('DOMContentLoaded', function () {
   const button = document.getElementById('getMessage')
 
   button.onclick = function () {
-    const req = new XMLHttpRequest()
-    req.open('GET', '/json/cats.json', true)
-    req.send()
-    req.onload = function () {
-      const json = JSON.parse(req.responseText)
-      document.getElementsByClassName('message')[0].innerHTML = JSON.stringify(json)
-    }
+    fetch('/json/cats.json')
+      .then((response) => response.json())
+      .then((json) => {
+        document.getElementById('message').innerHTML = JSON.stringify(json)
+      })
   }
 })
