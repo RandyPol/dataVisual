5ac1341400cf8e4e10cc88321de00ded311bba55
Ran Pol <rnd809@gmail.com>
2023-03-27 11:35:53 -0400

    Seeting up our line chart and establishing the accessor functions.

--- fullstackD3/chart.js
+++ fullstackD3/chart.js
@@ -1,5 +1,15 @@
 async function drawLineChart() {
   const dataSet = await d3.json('./my_weather_data.json')
-  console.log(dataSet)
+  // This is a function that takes a string and returns a date object
+
+  const dateParser = d3.timeParse('%Y-%m-%d')
+
+  //   These are the accessors for the data we want to use
+  const yAccessor = (d) => d.temperatureMax
+  const xAccessor = (d) => dateParser(d.date)
+
+  console.log(dataSet[0])
+  console.log(xAccessor(dataSet[0]))
 }
+
 drawLineChart()
