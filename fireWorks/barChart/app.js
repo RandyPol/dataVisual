5c7512202397123490f5dab852d8a99e84c86d0c
Ran Pol <rnd809@gmail.com>
2023-03-26 23:14:43 -0400

    Create the x and y scales functions.

--- fireWorks/barChart/app.js
+++ fireWorks/barChart/app.js
@@ -11,6 +11,21 @@ async function barChart() {
     .append('svg')
     .attr('width', width)
     .attr('height', height)
-}
 
+  //   Create Scales for the Bar Chart
+  //   xScale: Maps the data to the width of the canvas
+  const xScale = d3
+    .scaleBand()
+    .domain(['Arm', 'Leg', 'Head', 'Eye', 'Hand', 'Other'])
+    .range([0, width])
+
+  //  yScale: Maps the data to the height of the canvas
+  const yScale = d3
+    .scaleLinear()
+    .domain(d3.extent(dataSet, (d) => +d.Total))
+    .range([height, 0])
+
+  console.log(xScale('Arm'))
+  console.log(yScale(1700))
+}
 barChart()
