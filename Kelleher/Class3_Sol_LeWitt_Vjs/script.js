1ce597809d76117d151a2e6a4b9b46fa277fc105
Ran Pol <rnd809@gmail.com>
2023-04-01 17:22:08 -0400

    Create the for loop for the rect sol lewitt.

--- Kelleher/Class3_Sol_LeWitt_Vjs/script.js
+++ Kelleher/Class3_Sol_LeWitt_Vjs/script.js
@@ -1,1 +1,26 @@
-console.log('Hello world!')
+const width = window.innerWidth
+const height = window.innerHeight
+
+const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+svg.setAttribute('width', width)
+svg.setAttribute('height', height)
+document.body.appendChild(svg)
+
+
+const n = 100
+for (let i = 0; i < n; i++) {
+  const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect')
+  rect.setAttribute('y', i * 20)
+  rect.setAttribute('width', width)
+  rect.setAttribute('height', 10)
+  svg.appendChild(rect)
+}
+// const n = 100
+// for (let i = 0; i < n; i++) {
+//   const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect')
+//   rect.setAttribute('x', i * 20)
+//   rect.setAttribute('width', '10')
+//   rect.setAttribute('height', height)
+//   svg.appendChild(rect)
+// }
+console.log(svg.children.length)
