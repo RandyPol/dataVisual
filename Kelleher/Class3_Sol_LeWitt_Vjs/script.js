57db7db4cdb5b1551648b3619bd9bfb194265ef0
Ran Pol <rnd809@gmail.com>
2023-04-01 19:03:08 -0400

    Create a  Sol LeWitt in Vanilla JS.

--- Kelleher/Class3_Sol_LeWitt_Vjs/script.js
+++ Kelleher/Class3_Sol_LeWitt_Vjs/script.js
@@ -30,21 +30,48 @@ circle.setAttribute('r', 200)
 circle.setAttribute('fill', 'black')
 mask.appendChild(circle)
 
+/**
+ * Create a mask for the second for loop to create the lines
+ */
+
+// Create a mask
+const mask2 = document.createElementNS('http://www.w3.org/2000/svg', 'mask')
+mask2.setAttribute('id', 'circle-mask2')
+svg.appendChild(mask2)
+
+// Create a rectangle that will be used to create the mask
+const maskRect2 = document.createElementNS('http://www.w3.org/2000/svg', 'rect')
+maskRect2.setAttribute('width', width)
+maskRect2.setAttribute('height', height)
+maskRect2.setAttribute('fill', 'black')
+mask2.appendChild(maskRect2)
+
+// Create a circle that will be used to create the mask
+const circle2 = document.createElementNS('http://www.w3.org/2000/svg', 'circle')
+circle2.setAttribute('cx', width / 2)
+circle2.setAttribute('cy', height / 2)
+circle2.setAttribute('r', 200)
+circle2.setAttribute('fill', 'white')
+mask2.appendChild(circle2)
+
 const n = 100
+
+for (let i = 0; i < n; i++) {
+  const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect')
+  rect.setAttribute('x', i * 20)
+  rect.setAttribute('width', 10)
+  rect.setAttribute('height', height)
+  rect.setAttribute('mask', 'url(#circle-mask)')
+  svg.appendChild(rect)
+}
+
 for (let i = 0; i < n; i++) {
   const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect')
   rect.setAttribute('y', i * 20)
   rect.setAttribute('width', width)
   rect.setAttribute('height', 10)
-  rect.setAttribute('mask', 'url(#circle-mask)')
+  rect.setAttribute('mask', 'url(#circle-mask2)')
   svg.appendChild(rect)
 }
 
-// for (let i = 0; i < n; i++) {
-//   const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect')
-//   rect.setAttribute('x', i * 20)
-//   rect.setAttribute('width', '10')
-//   rect.setAttribute('height', height)
-//   svg.appendChild(rect)
-// }
 console.log(svg.children.length)
