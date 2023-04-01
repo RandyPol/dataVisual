5d02782050d182596fa94bfa6394dd8c502f49b1
Ran Pol <rnd809@gmail.com>
2023-04-01 18:41:17 -0400

    Set the first mask.

--- Kelleher/Class3_Sol_LeWitt_Vjs/script.js
+++ Kelleher/Class3_Sol_LeWitt_Vjs/script.js
@@ -6,6 +6,29 @@ svg.setAttribute('width', width)
 svg.setAttribute('height', height)
 document.body.appendChild(svg)
 
+/**
+ * Create a mask that will be used to create the lines
+ */
+
+// Create a mask
+const mask = document.createElementNS('http://www.w3.org/2000/svg', 'mask')
+mask.setAttribute('id', 'circle-mask')
+svg.appendChild(mask)
+
+// Create a rectangle that will be used to create the mask
+const maskRect = document.createElementNS('http://www.w3.org/2000/svg', 'rect')
+maskRect.setAttribute('width', width)
+maskRect.setAttribute('height', height)
+maskRect.setAttribute('fill', 'white')
+mask.appendChild(maskRect)
+
+// Create a circle that will be used to create the mask
+const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle')
+circle.setAttribute('cx', width / 2)
+circle.setAttribute('cy', height / 2)
+circle.setAttribute('r', 200)
+circle.setAttribute('fill', 'black')
+mask.appendChild(circle)
 
 const n = 100
 for (let i = 0; i < n; i++) {
@@ -13,9 +36,10 @@ for (let i = 0; i < n; i++) {
   rect.setAttribute('y', i * 20)
   rect.setAttribute('width', width)
   rect.setAttribute('height', 10)
+  rect.setAttribute('mask', 'url(#circle-mask)')
   svg.appendChild(rect)
 }
-// const n = 100
+
 // for (let i = 0; i < n; i++) {
 //   const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect')
 //   rect.setAttribute('x', i * 20)
