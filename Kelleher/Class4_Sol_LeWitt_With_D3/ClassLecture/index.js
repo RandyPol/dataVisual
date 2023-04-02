919d9dc4fa336e3a22509a3ccdbcfba199b19f87
Ran Pol <rnd809@gmail.com>
2023-04-02 15:14:46 -0400

    Add the symbol(s) module to generate the paths.

--- Kelleher/Class4_Sol_LeWitt_With_D3/ClassLecture/index.js
+++ Kelleher/Class4_Sol_LeWitt_With_D3/ClassLecture/index.js
@@ -1,31 +1,15 @@
-import { select, range } from 'd3'
+import { select, range, symbol, symbols } from 'd3'
 
 const width = window.innerWidth
 const height = window.innerHeight
 
-// const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
-// svg.setAttribute('width', width)
-// svg.setAttribute('height', height)
-// document.body.appendChild(svg)
 const svg = select('body')
   .append('svg')
   .attr('width', width)
   .attr('height', height)
 
 const n = 100
-// const marks = []
 
-// for (let i = 0; i < n; i++) {
-//   marks.push({
-//     y: i * 20,
-//     width: width,
-//     height: 10,
-//     mask: 'url(#circle-mask)',
-//   })
-// }
-
-// D3 Data Join Pattern
-// 1. Select the elements to bind data to
 svg
   .append('g')
   .selectAll('rect')
@@ -36,9 +20,6 @@ svg
   .attr('height', 10)
   .attr('mask', 'url(#circle-mask)')
 
-/**
- * Create the lines with the second mask
- */
 svg
   .append('g')
   .selectAll('rect')
@@ -53,74 +34,38 @@ svg
  * Create a mask that will be used to create the lines
  */
 
-// // Create a mask
-// const mask = document.createElementNS('http://www.w3.org/2000/svg', 'mask')
-// mask.setAttribute('id', 'circle-mask')
-// svg.appendChild(mask)
 const mask = svg.append('mask').attr('id', 'circle-mask')
-// // Create a rectangle that will be fill with white and will be used to visible the circle
-// const maskRect = document.createElementNS('http://www.w3.org/2000/svg', 'rect')
-// maskRect.setAttribute('width', width)
-// maskRect.setAttribute('height', height)
-// maskRect.setAttribute('fill', 'white')
-// mask.appendChild(maskRect)
+
 mask
   .append('rect')
   .attr('width', width)
   .attr('height', height)
-  .attr('fill', 'white')
-// // Create a circle that will be used to hide the lines
-// const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle')
-// circle.setAttribute('cx', width / 2)
-// circle.setAttribute('cy', height / 2)
-// circle.setAttribute('r', 200)
-// circle.setAttribute('fill', 'black')
-// mask.appendChild(circle)
-mask
-  .append('circle')
-  .attr('cx', width / 2)
-  .attr('cy', height / 2)
-  .attr('r', 200)
   .attr('fill', 'black')
+
+// We are going to append a path to the mask that will create the lines that we want
+// We are going to use the symbol generator to create the lines
+mask
+  .append('g')
+  .attr('transform', `translate(${width / 2}, ${height / 2})`)
+  .append('path')
+  .attr('d', symbol(symbols[2], 100000)())
+  .attr('fill', 'white')
 
 // /**
 //  * Create a mask for the second for loop to create the lines
 //  */
 
-// // Create a mask
-// const mask2 = document.createElementNS('http://www.w3.org/2000/svg', 'mask')
-// mask2.setAttribute('id', 'circle-mask2')
-// svg.appendChild(mask2)
 const mask2 = svg.append('mask').attr('id', 'circle-mask2')
-// //  Everything under a black pixel will be invisible
-// const maskRect2 = document.createElementNS('http://www.w3.org/2000/svg', 'rect')
-// maskRect2.setAttribute('width', width)
-// maskRect2.setAttribute('height', height)
-// maskRect2.setAttribute('fill', 'black')
-// mask2.appendChild(maskRect2)
+
 mask2
   .append('rect')
   .attr('width', width)
   .attr('height', height)
-  .attr('fill', 'black')
-// //  Everything under a white pixel will be visible
-// const circle2 = document.createElementNS('http://www.w3.org/2000/svg', 'circle')
-// circle2.setAttribute('cx', width / 2)
-// circle2.setAttribute('cy', height / 2)
-// circle2.setAttribute('r', 200)
-// circle2.setAttribute('fill', 'white')
-// mask2.appendChild(circle2)
+  .attr('fill', 'white')
+
 mask2
   .append('circle')
   .attr('cx', width / 2)
   .attr('cy', height / 2)
   .attr('r', 200)
-  .attr('fill', 'white')
-// for (let i = 0; i < n; i++) {
-//   const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect')
-//   rect.setAttribute('y', i * 20)
-//   rect.setAttribute('width', width)
-//   rect.setAttribute('height', 10)
-//   rect.setAttribute('mask', 'url(#circle-mask2)')
-//   svg.appendChild(rect)
-// }
+  .attr('fill', 'black')
