052704211fdc73db2dfcb5195f2b1980b348a844
Ran Pol <rnd809@gmail.com>
2023-04-02 15:36:39 -0400

    Add a function to refacture the creation of the masks.

--- Kelleher/Class4_Sol_LeWitt_With_D3/ClassLecture/index.js
+++ Kelleher/Class4_Sol_LeWitt_With_D3/ClassLecture/index.js
@@ -3,10 +3,45 @@ import { select, range, symbol, symbols } from 'd3'
 const width = window.innerWidth
 const height = window.innerHeight
 
+// Create a function to refactor the code
+const renderMask = (id, inverted) => {
+  const mask = svg.append('mask').attr('id', id)
+
+  mask
+    .append('rect')
+    .attr('width', width)
+    .attr('height', height)
+    .attr('fill', inverted ? 'white' : 'black')
+  // We are going to append a path to the mask that will create the lines that we want
+  // We are going to use the symbol generator to create the lines
+  // The fill white create the lines that we want to display on the screen and the rest of the mask will be black
+  mask
+    .append('g')
+    .attr('transform', `translate(${width / 2}, ${height / 2})`)
+    .append('path')
+    .attr('d', symbol(symbols[2], 100000)())
+    .attr('fill', inverted ? 'black' : 'white')
+
+  return mask
+}
+
+// Create the svg
 const svg = select('body')
   .append('svg')
   .attr('width', width)
   .attr('height', height)
+
+// Invoke the function to create the mask
+/**
+ * Create a mask for the first for loop to create the lines
+ */
+
+renderMask('mask-1', true)
+// /**
+//  * Create a mask for the second for loop to create the lines
+//  */
+
+renderMask('mask-2', false)
 
 const n = 100
 
@@ -29,44 +64,3 @@ svg
   .attr('width', 10)
   .attr('height', height)
   .attr('mask', 'url(#mask-2)')
-
-/**
- * Create a mask that will be used to create the lines
- */
-
-const mask = svg.append('mask').attr('id', 'mask-1')
-
-mask
-  .append('rect')
-  .attr('width', width)
-  .attr('height', height)
-  .attr('fill', 'black')
-
-// We are going to append a path to the mask that will create the lines that we want
-// We are going to use the symbol generator to create the lines
-// The fill white create the lines that we want to display on the screen and the rest of the mask will be black
-mask
-  .append('g')
-  .attr('transform', `translate(${width / 2}, ${height / 2})`)
-  .append('path')
-  .attr('d', symbol(symbols[2], 100000)())
-  .attr('fill', 'white')
-
-// /**
-//  * Create a mask for the second for loop to create the lines
-//  */
-
-const mask2 = svg.append('mask').attr('id', 'mask-2')
-
-mask2
-  .append('rect')
-  .attr('width', width)
-  .attr('height', height)
-  .attr('fill', 'white')
-
-mask2
-  .append('g')
-  .attr('transform', `translate(${width / 2}, ${height / 2})`)
-  .append('path')
-  .attr('d', symbol(symbols[2], 100000)())
-  .attr('fill', 'black')
