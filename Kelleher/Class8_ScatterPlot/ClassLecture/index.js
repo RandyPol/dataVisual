e491ed2e4b4090268e34dcf9c2679fe95c6cf278
Ran Pol <rnd809@gmail.com>
2023-04-03 11:57:40 -0400

    Create an Async functin to work with the data.

--- Kelleher/Class8_ScatterPlot/ClassLecture/index.js
+++ Kelleher/Class8_ScatterPlot/ClassLecture/index.js
@@ -1,6 +1,4 @@
-import { csv, select } from 'd3'
-
-// Step 1: Access data
+import { csv, select, selectAll } from 'd3'
 
 const csvUrl =
   'https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/iris.csv'
@@ -14,7 +12,20 @@ const parseRow = (row) => {
   return row
 }
 
-// csv takes a second argument which is a function that is called for each row
-csv(csvUrl, parseRow).then((data) => {
-  console.log(data)
-})
+//Create chart dimensions with an SVG element
+const width = window.innerWidth
+const height = window.innerHeight
+
+const svg = select('body')
+  .append('svg')
+  .attr('width', width)
+  .attr('height', height)
+
+const main = async () => {
+  // csv takes a second argument which is a function that is called for each row
+  const data = await csv(csvUrl, parseRow)
+
+  svg.selectAll('circle').data(data).join('circle').attr('r', 5)
+}
+
+main()
