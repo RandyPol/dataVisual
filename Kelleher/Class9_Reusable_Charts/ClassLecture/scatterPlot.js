79b9b186088c73d84aad566f07a01bb02977004f
Ran Pol <rnd809@gmail.com>
2023-04-04 00:21:53 -0400

    Pass the my function the selectioin argument and finish the implementation.

--- Kelleher/Class9_Reusable_Charts/ClassLecture/scatterPlot.js
+++ Kelleher/Class9_Reusable_Charts/ClassLecture/scatterPlot.js
@@ -1,3 +1,5 @@
+import { scaleLinear, extent, axisLeft, axisBottom } from 'd3'
+
 export const scatterPlot = (config) => {
   // Set default values for configuration properties
   let width = config.width || 720
@@ -8,7 +10,43 @@ export const scatterPlot = (config) => {
   let margin = config.margin || { top: 20, right: 20, bottom: 40, left: 50 }
   let radius = config.radius || 5
 
-  const my = () => {}
+  const my = () => {
+    const xScale = scaleLinear()
+      .domain(extent(data, xValue))
+      .range([margin.left, width - margin.right])
+
+    const yScale = scaleLinear()
+      .domain(extent(data, yValue))
+      .range([height - margin.bottom, margin.top])
+
+    // The marks are the coordinates of the data points in the chart dimensions and range
+    const marks = data.map((d) => {
+      return {
+        x: xScale(xValue(d)),
+        y: yScale(yValue(d)),
+      }
+    })
+
+    // Render the marks to the DOM
+    selection
+      .selectAll('circle')
+      .data(marks)
+      .join('circle')
+      .attr('cx', (d) => d.x)
+      .attr('cy', (d) => d.y)
+      .attr('r', radius)
+
+    // Add y-axis
+    selection
+      .append('g')
+      .attr('transform', `translate(${margin.left}, 0)`)
+      .call(axisLeft(yScale))
+    // Add x-axis
+    selection
+      .append('g')
+      .attr('transform', `translate(0, ${height - margin.bottom})`)
+      .call(axisBottom(xScale))
+  }
 
   my.width = function (_) {
     return arguments.length ? ((width = +_), my) : width
@@ -20,71 +58,17 @@ export const scatterPlot = (config) => {
     return arguments.length ? ((data = _), my) : data
   }
   my.xValue = function (_) {
-    return arguments.length ? ((xValue = _), my) : xValue
+    return arguments.length ? ((xValue = +_), my) : xValue
   }
   my.yValue = function (_) {
-    return arguments.length ? ((yValue = _), my) : yValue
+    return arguments.length ? ((yValue = +_), my) : yValue
   }
   my.margin = function (_) {
     return arguments.length ? ((margin = _), my) : margin
   }
   my.radius = function (_) {
-    return arguments.length ? ((radius = _), my) : radius
+    return arguments.length ? ((radius = +_), my) : radius
   }
 
-  // xScale is a function that takes a value and returns a pixel value
-  const xScale = scaleLinear()
-    .domain(extent(data, xValue))
-    // To make the x-axis to start from 0 we need to use the following domain instead
-    // .domain([0, extent(data, yValue)[1]])
-    .range([margin.left, width - margin.right])
-  console.log(xScale.domain())
-  console.log(xScale.range())
-
-  // yScale is a function that takes a value and returns a pixel value
-  const yScale = scaleLinear()
-    .domain(extent(data, yValue))
-    .range([height - margin.bottom, margin.top])
-
-  // The marks are the coordinates of the data points in the chart dimensions and range
-  const marks = data.map((d) => {
-    return {
-      x: xScale(xValue(d)),
-      y: yScale(yValue(d)),
-    }
-  })
-
-  // Render the marks to the DOM
-  svg
-    .selectAll('circle')
-    .data(marks)
-    .join('circle')
-    .attr('cx', (d) => d.x)
-    .attr('cy', (d) => d.y)
-    .attr('r', radius)
-
-  // Render the axes to the DOM
-  // yAxis
-  svg
-    .append('g')
-    .attr('transform', `translate(${margin.left}, 0)`)
-    .call(axisLeft(yScale))
-
-  /**
-
-     .call explains what the axis is doing:
-     const yAxis = axisLeft(yScale)
-     const yAxisGroup = svg.append('g').attr('transform', `translate(${margin.left}, 0)`)
-    //  We can then call the axis function on the group
-    yAxisGroup.call(yAxis) or yAxis(yAxisGroup)
-
-    When we use .call we are passing the group as the first argument to the function in this case the axis function yAxis and the group is the context of the function. This is the same as doing yAxis(yAxisGroup) or yAxisGroup.call(yAxis). The difference is that when we use .call we can chain the function calls and we can pass more arguments to the function. 
-      */
-
-  // xAxis
-  svg
-    .append('g')
-    .attr('transform', `translate(0, ${height - margin.bottom})`)
-    .call(axisBottom(xScale))
   return my
 }
