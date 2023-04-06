4f11c2d1e09c3acd36ced34621c4546ece198a90
Ran Pol <rnd809@gmail.com>
2023-04-05 22:01:40 -0400

    Restructure the containers for the divs.

--- Kelleher/Class11_Charts_Menu/ClassLecture/index.js
+++ Kelleher/Class11_Charts_Menu/ClassLecture/index.js
@@ -17,21 +17,24 @@ const parseRow = (row) => {
 }
 const width = window.innerWidth / 2
 const height = window.innerHeight / 2
-const svg = select('body')
-  .append('svg')
-  .attr('width', width)
-  .attr('height', height)
-const svg2 = select('body')
-  .append('svg')
-  .attr('width', width)
-  .attr('height', height)
-// Menu container is the div that will contain the menu
-const menuContainer = select('body')
-  .append('div')
-  .attr('class', 'menu-container')
 
+// Create the Dive container
+const divSvgContainer = select('#svgContainer')
+// Create First SVG and the sub elements
+const div = divSvgContainer.append('div').attr('class', 'divSvg-container')
+const menuContainer = div.append('div').attr('class', 'menu-container')
 const xMenu = menuContainer.append('div')
 const yMenu = menuContainer.append('div')
+
+const svg = div.append('svg').attr('width', width).attr('height', height)
+
+// Second SVG and the sub elements
+const div2 = divSvgContainer.append('div').attr('class', 'divSvgTwo-container')
+const menuContainer2 = div2.append('div').attr('class', 'menu-container')
+const xMenu2 = menuContainer2.append('div')
+const yMenu2 = menuContainer2.append('div')
+
+const svg2 = div2.append('svg').attr('width', width).attr('height', height)
 
 const main = async () => {
   const columns = ['petal_length', 'sepal_length', 'petal_width', 'sepal_width']
@@ -63,6 +66,8 @@ const main = async () => {
   // Call the menu function and pass the menuContainer as the selection
   xMenu.call(menu())
   yMenu.call(menu())
+  xMenu2.call(menu())
+  yMenu2.call(menu())
 
   svg.call(plot)
   svg2.call(plot2)
