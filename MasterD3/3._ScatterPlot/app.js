4c2e1db53693affc8c3b099c3defaf2df118fb65
Ran Pol <rnd809@gmail.com>
2023-04-08 00:06:17 -0400

    Create the starter file for the ScatterPlot.

---
+++ MasterD3/3._ScatterPlot/app.js
@@ -0,0 +1,16 @@
+import { select, json } from 'd3'
+
+const JSON_URL =
+  'https://gist.githubusercontent.com/RandyPol/177c1498022e0afaba65e50b9f3965b3/raw/e167bdc78f43cf44de6b3295f68f39f85960be0d/weatherData.json'
+
+const main = async () => {
+  try {
+    const data = await json(JSON_URL)
+    console.log(data.length)
+    console.log(data[0])
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+main()
