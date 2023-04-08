f9dcfa6acc7958052c501715724086acb5bec005
Ran Pol <rnd809@gmail.com>
2023-04-08 19:37:27 -0400

    Starter file for the heatmap.

---
+++ MasterD3/4._Scales/heatMap.js
@@ -0,0 +1,23 @@
+import { select, json } from 'd3'
+
+const heatData =
+  'https://gist.githubusercontent.com/RandyPol/2135ded73edfa16c69fa7d5ae92f9f8c/raw/18934cbbe9d586654f3e565c4cb4c3ac5ac0cf66/heatMapData.json'
+
+async function draw(el) {
+  // Data
+  const dataset = await json(heatData)
+
+  // Dimensions
+  let dimensions = {
+    width: 600,
+    height: 150,
+  }
+
+  // Draw Image
+  const svg = select(el)
+    .append('svg')
+    .attr('width', dimensions.width)
+    .attr('height', dimensions.height)
+}
+
+draw('#heatmap1')
