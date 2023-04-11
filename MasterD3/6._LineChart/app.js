d0c39bc13053635887660704b4b0037b68a5d2f0
Ran Pol <rnd809@gmail.com>
2023-04-11 12:27:19 -0400

    Starter files for lineChart.

---
+++ MasterD3/6._LineChart/app.js
@@ -0,0 +1,42 @@
+import { select, csv, scaleLinear, extent } from 'd3'
+
+async function draw() {
+  // Data
+  const dataset = await csv(
+    'https://gist.githubusercontent.com/RandyPol/2b3a261ddeb3c737eb0cb14e2517968a/raw/10192473b7fb4e7e205ef7e25f2cd6719d2f89ed/appleStock.csv'
+  )
+
+  const xAccessor = (d) => d.date
+  const yAccessor = (d) => d.close
+
+  // Dimensions
+  let dimensions = {
+    width: 1000,
+    height: 500,
+    margins: 50,
+  }
+
+  dimensions.ctrWidth = dimensions.width - dimensions.margins * 2
+  dimensions.ctrHeight = dimensions.height - dimensions.margins * 2
+
+  // Draw Image
+  const svg = select('#chart')
+    .append('svg')
+    .attr('width', dimensions.width)
+    .attr('height', dimensions.height)
+
+  const ctr = svg
+    .append('g') // <g>
+    .attr(
+      'transform',
+      `translate(${dimensions.margins}, ${dimensions.margins})`
+    )
+
+  // Scales
+  const yScale = scaleLinear()
+    .domain(extent(dataset, yAccessor))
+    .range([dimensions.ctrHeight, 0])
+    .nice()
+}
+
+draw()
