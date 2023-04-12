bf7c80e90d242478b65f73ad3db95234d09fe8ae
Ran Pol <rnd809@gmail.com>
2023-04-12 15:19:51 -0400

    Formmating the piechart data.

--- MasterD3/7._PieCharts/app.js
+++ MasterD3/7._PieCharts/app.js
@@ -1,10 +1,9 @@
-import { select, csv } from 'd3'
+import { select, csv, pie } from 'd3'
 async function draw() {
   // Data
   const dataset = await csv(
     'https://gist.githubusercontent.com/RandyPol/a6f7cf4b2471f0ca189cfe0cb0681168/raw/e48af0e07289f7a7cb2e250f94d32e825dccf91e/population.csv'
   )
-
 
   // Dimensions
   let dimensions = {
@@ -30,6 +29,10 @@ async function draw() {
     )
 
   // Scales
+  const populationPie = pie().value((d) => d.value)
+  const slices = populationPie(dataset)
+
+  console.log(slices)
 }
 
 draw()
