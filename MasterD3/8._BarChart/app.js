7f97319b252b175a60ec04dfc47beb80fccd4a55
Ran Pol <rnd809@gmail.com>
2023-04-12 22:55:29 -0400

    Adding color.

--- MasterD3/8._BarChart/app.js
+++ MasterD3/8._BarChart/app.js
@@ -1,4 +1,14 @@
-import { csv, select, autoType, stack, scaleLinear, max, scaleBand } from 'd3'
+import {
+  csv,
+  select,
+  autoType,
+  stack,
+  scaleLinear,
+  max,
+  scaleBand,
+  scaleOrdinal,
+  schemeSpectral,
+} from 'd3'
 
 async function draw() {
   // Data
@@ -49,6 +59,29 @@ async function draw() {
   const xScale = scaleBand()
     .domain(dataset.map((state) => state.name))
     .range([dimensions.margins, dimensions.ctrWidth])
+
+  const colorScale = scaleOrdinal()
+    .domain(stackData.map((d) => d.key))
+    .range(schemeSpectral[stackData.length])
+    .unknown('#ccc')
+
+  // Draw Bars
+  const ageGroups = ctr
+    .append('g')
+    .classed('age-groups', true)
+    .selectAll('g')
+    .data(stackData)
+    .join('g')
+    .attr('fill', (d) => colorScale(d.key))
+
+  ageGroups
+    .selectAll('rect')
+    .data((d) => d)
+    .join('rect')
+    .attr('x', (d) => xScale(d.data.name))
+    .attr('y', (d) => yScale(d[1]))
+    .attr('height', (d) => yScale(d[0]) - yScale(d[1]))
+    .attr('width', xScale.bandwidth())
 }
 
 draw()
